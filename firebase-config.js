1609ceef48d30b55b6342d45fa74fb62ed1ff8a3
satheeshrock16-ship-it <satheeshrock16@gmail.com>
2026-09-20 00:39:25 +0530

    Add Firebase configuration template

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Calorie Duo Firebase configuration.
+// Get these values from Firebase Console -> Project settings -> Your apps -> Web app.
+// Firebase web config is designed to be public; database rules protect the data.
+window.CALORIE_DUO_FIREBASE_CONFIG={
+  apiKey:"PASTE_API_KEY",
+  authDomain:"PASTE_PROJECT_ID.firebaseapp.com",
+  projectId:"PASTE_PROJECT_ID",
+  storageBucket:"PASTE_PROJECT_ID.firebasestorage.app",
+  messagingSenderId:"PASTE_MESSAGING_SENDER_ID",
+  appId:"PASTE_APP_ID"
+};
